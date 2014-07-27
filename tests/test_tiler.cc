f949fff18afb2634417ebd38ea4a7a184ea524b0
Jeff Perry <jeffsp@gmail.com>
2014-07-27 13:56:17 -0500

    add draw() routine

--- tests/test_tiler.cc
+++ tests/test_tiler.cc
@@ -37,33 +37,22 @@ void test0 ()
     VERIFY (l.size () == 40);
 }
 
-void test1 ()
+void draw (unsigned w, unsigned h, const points &locs, const polygons &all_polys)
 {
-    unsigned w = 200;
-    unsigned h = 200;
-    const polygon screen { point (0, 0), point (w, 0), point (w, h), point (0, h) };
     unsigned border = 100;
     rgb8_image_t img (h + border * 2, w + border * 2);
     img.assign (255);
 
-    double tw = 40;
-    double th = 40;
-    double angle = 10;
-    auto l = get_tile_locations (h, w, point (w / 2.0, h / 2.0), tw, th, angle, false);
-
-    const polygons polys { polygon { point (0, 0), point (tw, 0), point (tw, th), point (0, th) } };
-
-    auto g = get_tiled_polygons (l, polys, angle);
-
     // draw polygons
-    for (auto i : g)
+    for (auto i : all_polys)
         draw_lines (img, i + point (border, border), rgb8_pixel_t ({212, 212, 212}));
 
     // draw screen outline
+    const polygon screen { point (0, 0), point (w, 0), point (w, h), point (0, h) };
     draw_lines (img, screen + point (border, border), rgb8_pixel_t ({200, 200, 255}));
 
     // draw red dots at tile locations
-    for (auto i : l)
+    for (auto i : locs)
     {
         point pt = i + point (border, border);
         size_t px = round (pt.x);
@@ -78,8 +67,22 @@ void test1 ()
 
     const char *name = "test";
     cv::imshow (name, image_to_mat (img));
+    cv::moveWindow (name, 200, 200);
     cv::waitKey (0);
     cv::destroyWindow (name);
+}
+
+void test1 ()
+{
+    unsigned w = 200;
+    unsigned h = 200;
+    double tw = 40;
+    double th = 40;
+    double angle = 10;
+    auto locs = get_tile_locations (h, w, point (w / 2.0, h / 2.0), tw, th, angle, false);
+    const polygons tile_polys { polygon { point (0, 0), point (tw, 0), point (tw, th), point (0, th) } };
+    auto all_polys = get_tiled_polygons (locs, tile_polys, angle);
+    draw (w, h, locs, all_polys);
 }
 
 void test2 ()
@@ -87,47 +90,16 @@ void test2 ()
     unsigned w = 200;
     unsigned h = 200;
     const polygon screen { point (0, 0), point (w, 0), point (w, h), point (0, h) };
-    unsigned border = 100;
-    rgb8_image_t img (h + border * 2, w + border * 2);
-    img.assign (255);
 
     floret_pentagonal p;
     double scale = 30;
     double tw = scale * p.get_width ();
     double th = scale * p.get_height ();
     double angle = 10;
-    auto l = get_tile_locations (h, w, point (w / 2.0, h / 2.0), tw, th, angle, true);
-
-    polygons polys = p.get_polygons ();
-    for (auto &i : polys)
-        i = ::scale (i, scale);
-    auto g = get_tiled_polygons (l, polys, angle);
-
-    // draw polygons
-    for (auto i : g)
-        draw_lines (img, i + point (border, border), rgb8_pixel_t ({212, 212, 212}));
-
-    // draw screen outline
-    draw_lines (img, screen + point (border, border), rgb8_pixel_t ({200, 200, 255}));
-
-    // draw red dots at tile locations
-    for (auto i : l)
-    {
-        point pt = i + point (border, border);
-        size_t px = round (pt.x);
-        size_t py = round (pt.y);
-        if (px < img.cols () && py < img.rows ())
-        {
-            img (py, px, 0) = 255;
-            img (py, px, 1) = 0;
-            img (py, px, 2) = 0;
-        }
-    }
-
-    const char *name = "test";
-    cv::imshow (name, image_to_mat (img));
-    cv::waitKey (0);
-    cv::destroyWindow (name);
+    auto locs = get_tile_locations (h, w, point (w / 2.0, h / 2.0), tw, th, angle, p.is_triangular ());
+    const polygons tile_polys = ::scale (p.get_polygons (), scale);
+    auto all_polys = get_tiled_polygons (locs, tile_polys, angle);
+    draw (w, h, locs, all_polys);
 }
 
 int main ()
